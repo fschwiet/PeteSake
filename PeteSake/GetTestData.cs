5474a7899fc0425aee6f3e228b7cd98cff7f6063
fschwiet <fschwiet@gmail.com>
2012-10-05 20:01:25 -0700

    dump query to XML

---
+++ PeteSake/GetTestData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using ManyConsole;
+
+namespace PeteSake
+{
+    public class GetTestData : ConsoleCommand
+    {
+        public GetTestData()
+        {
+            this.IsCommand("get-test", "Downloads SQL results and writes them to json");
+            this.HasRequiredOption("p=", "Base filename used to store query results.", v => BaseFilepath = v);
+            this.HasRequiredOption("c=", "Connection string with default database set.", v => SqlConnectionString = v);
+            this.HasRequiredOption("q=", "Query to execute (filepath or actual query text).", v => Query = v);
+        }
+
+        public string BaseFilepath;
+        public string SqlConnectionString;
+        public string Query;
+
+        public override int Run(string[] remainingArguments)
+        {
+            var queryAsLocalFile = new Uri(Query).LocalPath;
+
+            if (queryAsLocalFile != null)
+                Query = File.ReadAllText(Query);
+
+            using(var connection = new SqlConnection(SqlConnectionString))
+            {
+                connection.Open();
+
+                using(var command = new SqlCommand(Query, connection))
+                {
+                    var result = command.ExecuteXmlReader();
+
+                    var doc = XDocument.Load(result);
+
+                    Console.WriteLine(doc.ToString());
+                }
+            }
+
+            return 0;
+        }
+    }
+}
