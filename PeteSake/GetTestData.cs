55e1f5a51c248755fcb0f7717dae3f5a7a351197
fschwiet <fschwiet@gmail.com>
2012-10-05 22:16:28 -0700

    better json dumping

--- PeteSake/GetTestData.cs
+++ PeteSake/GetTestData.cs
@@ -18,7 +18,7 @@ namespace PeteSake
         public GetTestData()
         {
             this.IsCommand("save-test-json", "Downloads SQL results and writes them to json.");
-            this.HasRequiredOption("=", "Base filename used to store query results.", v => BaseFilepath = v);
+            this.HasRequiredOption("p=", "Base filename used to store query results.", v => BaseFilepath = v);
             this.HasRequiredOption("c=", "Connection string with default database set.", v => SqlConnectionString = v);
             this.HasRequiredOption("q=", "Query to execute (filepath or actual query text) (should produce an XML result -> add 'for XML auto' to the end of SELECT).", v => Query = v);
         }
@@ -57,25 +57,20 @@ namespace PeteSake
                         {
                             var xmlElement = XElement.Parse(reader.ReadOuterXml());
 
-                            foreach(var attribute in xmlElement.Attributes())
-                            {
-                                if (attribute.Name.LocalName.StartsWith("@"))
-                                {
-                                    attribute.Remove();
-                                    xmlElement.Add(new XAttribute(attribute.Name.LocalName.TrimStart('@'), attribute.Value));
-                                }
-                            }
-
                             var rowName = xmlElement.Name.LocalName;
 
                             if (!openFiles.ContainsKey(rowName))
                             {
-                                openFiles[rowName] = new StreamWriter(File.OpenWrite(BaseFilepath + "." + rowName + ".json.txt"));
+                                openFiles[rowName] = new StreamWriter(File.Open(BaseFilepath + "." + rowName + ".json.txt", FileMode.OpenOrCreate));
                                 openFiles[rowName].WriteLine("[");
                             }
 
-                            openFiles[rowName].WriteLine(JsonConvert.SerializeXNode(xmlElement, Formatting.Indented, true) + ",");
-                            //openFiles[rowName].WriteLine(xmlElement + ",");
+                            Dictionary<string, object> row= new Dictionary<string, object>();
+
+                            foreach (var attribute in xmlElement.Attributes())
+                                row[attribute.Name.LocalName] = attribute.Value;
+
+                            openFiles[rowName].WriteLine(JsonConvert.SerializeObject(row, Formatting.Indented) + ",");
                         }
                     }
                 }
