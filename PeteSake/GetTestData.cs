52f50ec2491efb8ad7f6b01d034b1a2e067e504c
fschwiet <fschwiet@gmail.com>
2012-10-05 21:45:08 -0700

    dumping the data in json... kinda weirdish

--- PeteSake/GetTestData.cs
+++ PeteSake/GetTestData.cs
@@ -5,8 +5,11 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 using ManyConsole;
+using Newtonsoft.Json;
+using Formatting = Newtonsoft.Json.Formatting;
 
 namespace PeteSake
 {
@@ -15,9 +18,9 @@ namespace PeteSake
         public GetTestData()
         {
             this.IsCommand("save-test-json", "Downloads SQL results and writes them to json.");
-            this.HasRequiredOption("p=", "Base filename used to store query results.", v => BaseFilepath = v);
+            this.HasRequiredOption("=", "Base filename used to store query results.", v => BaseFilepath = v);
             this.HasRequiredOption("c=", "Connection string with default database set.", v => SqlConnectionString = v);
-            this.HasRequiredOption("q=", "Query to execute (filepath or actual query text).", v => Query = v);
+            this.HasRequiredOption("q=", "Query to execute (filepath or actual query text) (should produce an XML result -> add 'for XML auto' to the end of SELECT).", v => Query = v);
         }
 
         public string BaseFilepath;
@@ -40,18 +43,49 @@ namespace PeteSake
             {
             }
 
+            Dictionary<string, StreamWriter> openFiles = new Dictionary<string, StreamWriter>();
+
             using(var connection = new SqlConnection(SqlConnectionString))
             {
                 connection.Open();
 
                 using(var command = new SqlCommand(Query, connection))
                 {
-                    var result = command.ExecuteXmlReader();
+                    using(var reader = command.ExecuteXmlReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var xmlElement = XElement.Parse(reader.ReadOuterXml());
 
-                    var doc = XDocument.Load(result);
+                            foreach(var attribute in xmlElement.Attributes())
+                            {
+                                if (attribute.Name.LocalName.StartsWith("@"))
+                                {
+                                    attribute.Remove();
+                                    xmlElement.Add(new XAttribute(attribute.Name.LocalName.TrimStart('@'), attribute.Value));
+                                }
+                            }
 
-                    Console.WriteLine(doc.ToString());
+                            var rowName = xmlElement.Name.LocalName;
+
+                            if (!openFiles.ContainsKey(rowName))
+                            {
+                                openFiles[rowName] = new StreamWriter(File.OpenWrite(BaseFilepath + "." + rowName + ".json.txt"));
+                                openFiles[rowName].WriteLine("[");
+                            }
+
+                            openFiles[rowName].WriteLine(JsonConvert.SerializeXNode(xmlElement, Formatting.Indented, true) + ",");
+                            //openFiles[rowName].WriteLine(xmlElement + ",");
+                        }
+                    }
                 }
+            }
+
+            foreach(var file in openFiles.Values)
+            {
+                file.WriteLine("]");
+                file.Flush();
+                file.Close();
             }
 
             return 0;
